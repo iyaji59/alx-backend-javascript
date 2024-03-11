637c96ba035956db16217dd2792af2b6211af579
Tonybruce <71387604+iyaji59@users.noreply.github.com>
2024-03-11 04:39:10 +0000

    correcting eslint

--- 0x00-ES6_basic/test/0-main.js
+++ 0x00-ES6_basic/test/0-main.js
@@ -1,3 +1,3 @@
-import { taskFirst, taskNext } from "../0-constants";
-//The ${...} syntax inside the template literal allows you to embed expressions, including function calls
+import { taskFirst, taskNext } from '../0-constants';
+
 console.log(`${taskFirst()} ${taskNext()}`);
