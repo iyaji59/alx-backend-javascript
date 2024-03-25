309a2448a5c04e82c2e3518487ca86ab6c911cbe
Tonybruce <71387604+iyaji59@users.noreply.github.com>
2024-03-25 01:53:55 +0000

    advance task

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -12,3 +12,10 @@ export default function guardrail(mathFunction) {
 
   return queue;
 }
+/*We define a function guardrail that accepts a single argument mathFunction, which is a function.
+Inside guardrail, we initialize an empty array named queue.
+We use a try-catch-finally block:
+In the try block, we execute the mathFunction and push its result to the queue.
+If an error occurs during the execution of mathFunction, the catch block catches the error, extracts its message, and pushes it to the queue.
+The finally block always executes, regardless of whether an error occurred or not. We push the message 'Guardrail was processed' to the queue.
+Finally, we return the queue array.*/
